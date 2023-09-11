09502877f44c96f4868cce5c034727bd8be463d4
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2023-09-11 06:55:05 +0000

    add urls

--- grocery/settings.py
+++ grocery/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '8000-rayyar-b2c-4srcbpr8ie5.ws-us104.gitpod.io']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '8000-rayyar-b2c-b2wm0jo2fsf.ws-eu104.gitpod.io']
 
 
 # Application definition
--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .views import ProductsView
 
 urlpatterns = [
-    path('', ProductsView.as_view(), name='products-view'),
+    path('', ProductsView.as_view(), name='base_url'),
+    path('products/', ProductsView.as_view(), name='products_url'),
+    path('about-us/', ProductsView.as_view(), name='about_us_url'),
+    path('contact-us/', ProductsView.as_view(), name='contact_us_url'),
+    path('whishlist/', ProductsView.as_view(), name='wishlist_url'),
+    path('checkout/', ProductsView.as_view(), name='checkout_url'),
 ]
