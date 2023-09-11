61a51842159e8f6d01d4ebdac6dd75301256e7d9
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2023-09-11 12:53:25 +0000

    add static css path

--- products/views.py
+++ products/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from django.contrib import messages
+from .models import Product
 
 
-class ProductsView(View):
+class ProductsView(generic.ListView):
     template_name = "index.html"
-    
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+
+    model = Product
+    object = Product.objects.order_by('created_at')
+    paginate_by = 8
+
