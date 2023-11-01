184d85233ea92858d2150aa1d5389d4a0da39ea8
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2023-11-01 07:37:59 +0000

    add product in bag

--- grocery/settings.py
+++ grocery/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '8000-rayyar-b2c-ozzq98xji8v.ws-us105.gitpod.io']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '8000-rayyar-b2c-rg3sxah8e1j.ws-us105.gitpod.io']
 
 
 # Application definition
