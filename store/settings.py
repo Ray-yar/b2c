e5fc5751ebb87803cbadc8e2dd9103897ff41afc
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2024-03-11 11:42:06 +0000

    Add reviewer name

--- store/settings.py
+++ store/settings.py
@@ -212,7 +212,7 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = os.environ.get('G_ACCOUNT')
+    EMAIL_HOST_PASSWORD = os.environ.get('APP_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('G_ACCOUNT')
 
