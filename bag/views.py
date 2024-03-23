16f4e7f270a5c2507d9bbcc35f2886c87197c463
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2024-03-23 14:11:18 +0000

    Fix notification issues.

--- bag/views.py
+++ bag/views.py
@@ -1,22 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from products.models import Product
 
 def view_bag(request):
     return render(request, 'components/bag/bag.html')
 
-
-
 def add_to_bag(request, item_id):
+
     bag = request.session.get('bag', {})
-    
+    product = get_object_or_404(Product, id=item_id)
     quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.title} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.title} to your bag')
     
-    messages.success(request, 'Successfully added into the shopping bag!')
-
     request.session['bag'] = bag
-    redirect_url = request.POST.get('redirect_url')
     return redirect(redirect_url)
