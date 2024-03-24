2ef908cab79959ff3b483ce5befbdd077ab4d27f
rohullah-celiver <rohullah.ahmadyar@celiver.com>
2024-03-24 16:13:15 +0000

    create home screen

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='base_url'),
+    path('', views.home, name='base_url'),
     path('products/', views.all_products, name='products_url'),
     path('about-us/', views.about_us, name='about_us_url'),
     path('contact-us/', views.contact_us, name='contact_us_url'),
