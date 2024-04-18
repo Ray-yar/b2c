9e2a404ac86bf506a51602bc10e4b3967c98bdac
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2024-04-18 16:03:58 +0000

    deploy

--- store/settings.py
+++ store/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'b2c-7a93138c14a4.herokuapp.com', '8000-rayyar-b2c-dq6muyzfavg.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'b2c-7a93138c14a4.herokuapp.com', '8000-rayyar-b2c-et77qijux3x.ws-eu110.gitpod.io']
 
 
 # Application definition
