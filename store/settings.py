d96b0709a222ed897237fe7e1510ee85151b0f88
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2024-10-06 20:46:01 +0000

    change debug to true

--- store/settings.py
+++ store/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '8000-rayyar-b2c-vp3e5eh379l.ws-eu116.gitpod.io', "b2c-5f721a629a2c.herokuapp.com/"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '8000-rayyar-b2c-vp3e5eh379l.ws-eu116.gitpod.io', "b2c-5f721a629a2c.herokuapp.com"]
 
 
 # Application definition
